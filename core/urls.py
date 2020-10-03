046318d40b002b69629e556c2e8b2e29b7d6ed19
anushapant <anusha.pant@gmail.com>
2020-10-03 11:35:12 +0530

    Requirements modified

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,10 @@ app_name = 'core'
 
 urlpatterns = [
     path('', Home_V.as_view(), name = 'home'),
-    path('index.html', Home_V.as_view(), name='home'),
-    path('shop.html', shop, name='shop'),
-    path('checkout.html', checkout, name='checkout'),
-    path('shop-cart.html', cart, name='cart'),
-    path('product-details.html/<slug>/', product_details_V.as_view(), name='product_details'),
-    path('contact.html', contact_us, name='contact_us')
+    path('index', Home_V.as_view(), name='home'),
+    path('shop', shop, name='shop'),
+    path('checkout', checkout, name='checkout'),
+    path('shop-cart', cart, name='cart'),
+    path('product-details/<slug>/', product_details_V.as_view(), name='product_details'),
+    path('contact', contact_us, name='contact_us')
 ]
